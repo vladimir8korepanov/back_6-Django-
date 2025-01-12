36254e20c5ff88818524abc335bd99c27a25f11d
vladimir8korepanov <vova.korepanov1999@mail.ru>
2025-01-12 20:57:10 +0400

    first commit

---
+++ my_blog/blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return "Post - {}".format(self.title)
+     
+# Create your models here.
---
+++ my_blog/blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('posts/', views.post_list, name='post_list'),
+    path('posts/<int:pk>', views.post_detail, name='post_detail'),
+]
